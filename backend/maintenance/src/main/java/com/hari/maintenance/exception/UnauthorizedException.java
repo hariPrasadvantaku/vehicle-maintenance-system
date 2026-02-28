0c95dc9994f0d7d2b6cc073fa3c74b9db44743dc
Hari Prasad <hariprasadvantaku@gmail.com>
2026-02-28 17:00:40 +0530

    Add global exception handling with custom exceptions for improved API error responses

---
+++ backend/maintenance/src/main/java/com/hari/maintenance/exception/UnauthorizedException.java
@@ -0,0 +1,10 @@
+package com.hari.maintenance.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+	public UnauthorizedException(String message) {
+		super(message);
+		
+	}
+
+}
---
+++ backend/maintenance/src/main/java/com/hari/maintenance/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.hari.maintenance.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+	
+}
